3514470637ca3ec92bb0d1073935676977db763a
Julio Sansossio <juliosansossio@gmail.com>
2021-06-22 17:47:53 +0200

    turn on/off using motion sensor

---
+++ hcsr501-plus-relay/hcsr501-plus-relay.ino
@@ -0,0 +1,33 @@
+#define sensorPin 2
+#define relayPin 3
+
+int lastSensorValue;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("V2");
+  pinMode(sensorPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+
+  digitalWrite(relayPin, LOW);
+}
+
+void loop()
+{
+  int sensorValue = digitalRead(sensorPin);
+  if (lastSensorValue != sensorValue)
+  {
+    lastSensorValue = sensorValue;
+    if (sensorValue == 1)
+    {
+      Serial.println("-> OPEN");
+      digitalWrite(relayPin, LOW);
+    }
+    else
+    {
+      Serial.println("-> CLOSE");
+      digitalWrite(relayPin, HIGH);
+    }
+  }
+}
